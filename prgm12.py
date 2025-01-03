ca81d38ec7dfda8f930cbde05c5522169b3dbff3
RASHID A K <rashidakrashidak@gmail.com>
2025-01-03 20:29:35 +0530

    Update prgm12.py

--- prgm12.py
+++ prgm12.py
@@ -1,4 +1,11 @@
-#PRINT * 
+"""
+PROGRAM TO PRINT PATTRN OF * IN A GRID WITH n NUMBER OF COLUMNS AND ROWS
+EX:
+    N=3
+    * * *
+    * * *
+    * * *
+"""
 n=int(input("ENTER THE NUMBER:"))
 for i in range(n):
     for j in range(n):
