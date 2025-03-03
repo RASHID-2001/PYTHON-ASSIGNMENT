3461a27e7b9a92099111c24b576b3147c9880f4d
RASHID A K <rashidakrashidak@gmail.com>
2025-03-03 13:00:04 +0530

    Add files via upload

--- prgm48.py
+++ prgm48.py
@@ -1,21 +1,15 @@
 """
-UNIVERSITY QUESTION::JUNE 2022
-Write a Python program to read n integers into a list and separate the positive
-and negative numbers into two different lists.?
+QN:How to draw a star shape using turtle in Python.(MAY 2022)
 
 """
-n=int(input("ENTER THE NUMBER OF ELEMENTS IN LIST:"))
-l=[]
-for i in range(n):
-	k=int(input(f"ENTER THE {i}th ELEMENT:"))
-	l.append(k)
-print(l)
-l_n=[]
-l_p=[]
-for i in range(n):
-	if l[i]<0:
-		l_n.append(l[i])
-	else:
-		l_p.append(l[i])
-print("NEGETIVE NUMBERS:"l_n)
-print("POSITIVE NUMBERS:"l_p)
+import turtle
+t = turtle.Turtle()
+t.pencolor("blue")
+t.speed(3)
+
+
+for _ in range(5):
+    t.forward(150)  
+    t.right(144)   
+t.hideturtle()
+turtle.done()
--- prgm53.py
+++ prgm53.py
@@ -1,22 +1,12 @@
 """
-UNIVERSITY QUESTION:JUNE 2023
-Assume that there is a text file named “numbers.txt”. Write a python program to find the median of list of numbers in the file without using standard function for median.
-
+QN:Write a python function to draw a hexagon using turtle graphics(JUNE 2023)
 """
-f = open("numbers.txt", "r")  
-lis = []
-for line in f:
-    	lis.extend(map(int, line.split()))  
-
-f.close() 
-
-lis.sort()  
-
-n = len(lis)
-if n % 2 == 1:
-	md_ind = n // 2  
-    	print("Median:", lis[md_ind])
-else:
-    	md_ind = n // 2
-    	print("Median:", (lis[md_ind] + lis[md_ind - 1]) / 2)
-
+import turtle
+def draw_square(t,side_length):
+	for _ in range(6):  
+        	t.forward(side_length)          
+		t.left(6)  
+t = turtle.Turtle()     
+draw_square(t,100)
+t.hideturtle() 
+turtle.done()
