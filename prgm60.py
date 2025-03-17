b712e094e694123bd56476a047c16ba1afbbb437
RASHID A K <rashidakrashidak@gmail.com>
2025-03-17 20:23:05 +0530

    Add files via upload

---
+++ prgm60.py
@@ -0,0 +1,23 @@
+"""
+QN:How to draw a square and hexagon using turtle module in Python.(MAY 2024)
+
+"""
+# TO DRAW A SQUARE
+import turtle
+
+t = turtle.Turtle()
+
+
+for _ in range(4):
+    t.forward(100)  
+    t.left(90)      
+
+ #TO DRAW HEXAGON
+t.up()
+t.left(180)
+t.forward(150)
+t.down()
+for _ in range(6):
+    t.forward(100)  
+    t.left(60)      
+turtle.done()
---
+++ prgm61.py
@@ -0,0 +1,10 @@
+""""
+QN:Write a Python program to draw a hexagon using turtle graphics(JANUARY 2024)
+"""
+import turtle
+t=turtle.Turtle()
+for i in range(6):
+	t.forward(150)
+	t.left(60)
+t.hideturtle()
+turtle.done()
